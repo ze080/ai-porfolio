ad21791329031b7acc1597882e2479d7ba225c3b
zayed binjad <ze080@zayeds-MacBook-Pro.local>
2025-04-07 14:19:41 +0530

    Removed theme switcher for clean build

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import { setInitialTheme } from './theme';
+//import { setInitialTheme } from './theme';
 
 // Apply theme before rendering
-setInitialTheme();
+//setInitialTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
