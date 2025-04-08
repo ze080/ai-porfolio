932d0cfad7c0b46e6df07645f7975c147590c060
zayed binjad <ze080@zayeds-MacBook-Pro.local>
2025-04-08 23:10:09 +0530

    🔥 Enhanced blog layout + Week 0 post improvements

--- src/blogs/Week1.jsx
+++ src/blogs/Week1.jsx
@@ -7,7 +7,7 @@ const Week1 = () => {
   return (
     <BlogLayout title="🧠 Week 1: Foundations of AI - Python, Math & Mindset" date="April 13, 2025">
       <div className="space-y-6 text-gray-200">
-        <p>Coming on next 13th April i.e. Sunday @ 9PM so stay tuned !! </p>
+        <p>Coming on 13th April i.e. Sunday @ 9PM so stay tuned !! </p>
         {/* Your blog content here */}
       </div>
     </BlogLayout>
